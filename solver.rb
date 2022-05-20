e954482ad2a50a3071161bdeb4b5502e5de16afb
Dire Akanbi <direakanbi@icloud.com>
2022-05-20 12:31:55 +0100

    Update solver.rb

--- solver.rb
+++ solver.rb
@@ -9,6 +9,19 @@ class Solver
   def reverse(word)
     word.reverse.to_s
   end
+  
+  def fizzbuzz(number)
+    numb = number.to_i
+    if numb % 3 == 0 && numb % 5 == 0
+      'fizzbuzz'
+    elsif numb % 3 == 0
+      'fizz'
+    elsif numb % 5 == 0
+      'buzz'
+    else
+      numb
+    end
+  end
 end
 
 print Solver.new.factorial(3)
